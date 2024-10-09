16fffbd4dfdd647d2e5874b82f0e3b4fb3c34e57
dungna <dungna@gmail.com>
2024-10-09 11:43:36 +0700

    remove model

--- app/Console/Commands/TonPeriodicDepositTransactionCommand.php
+++ app/Console/Commands/TonPeriodicDepositTransactionCommand.php
@@ -2,10 +2,14 @@
 
 namespace App\Console\Commands;
 
+use App\Models\WalletTonTransaction;
 use Illuminate\Console\Command;
+use App\Traits\ClientTrait;
 
 class TonPeriodicDepositTransactionCommand extends Command
 {
+    use ClientTrait;
+
     /**
      * The name and signature of the console command.
      *
@@ -25,6 +29,13 @@ class TonPeriodicDepositTransactionCommand extends Command
      */
     private string $baseUri;
 
+    public function __construct()
+    {
+        $this->baseUri = config('services.ton.is_main') ? config('services.ton.base_uri_ton_api_main') :
+            config('services.ton.base_uri_ton_api_test');
+        parent::__construct();
+    }
+
     /**
      * Execute the console command.
      *
@@ -32,10 +43,25 @@ class TonPeriodicDepositTransactionCommand extends Command
      */
     public function handle()
     {
+        $params = ['limit' => 25, 'sort_order' => 'ASC'];
+        $afterLt = 0;
         while (true) {
             echo "get new transactions ... \n";
+            $theLastWalletTransaction = WalletTonTransaction::orderBy('lt', "DESC")->first();
+            if (!$theLastWalletTransaction) {
+                $afterLt = $theLastWalletTransaction->lt;
+            }
+            if ($afterLt) {
+                $params['after_lt'] = $afterLt;
+            }
+            $basePath = $this->baseUri . "v2/blockchain/accounts/" . config('services.ton.root_ton_wallet') .
+                "/transactions";
+            $query = http_build_query($params);
+            $results = $this->httpGet($basePath . '?' . $query);
+            if ($results['status'] != 200) {
+                continue;
+            }
             sleep(1);
-
         }
         return Command::SUCCESS;
     }
